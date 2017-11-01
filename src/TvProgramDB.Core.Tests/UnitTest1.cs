5549fc7f4db822df16e7b969299da144083091a3
Pierre Plagnes <pierre.plagnes@gmail.com>
2017-11-01 11:02:41 +0100

    source

---
+++ src/TvProgramDB.Core.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using TvProgramDB.WebApi;
+using Xunit;
+
+namespace TvProgramDB.Core.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
---
+++ src/TvProgramDB.Crawler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TvProgramDB.Crawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
